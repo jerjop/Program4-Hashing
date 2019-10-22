622bf3203d03a51aea2b4493d8820506f28e8767
jerjop <jerjop@gmail.com>
2019-10-21 23:00:16 -0600

    hopefully adding a lot of the meat of the assignment

--- src/Reviews.java
+++ src/Reviews.java
@@ -1,26 +1,22 @@
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Reviews {
 
     public Reviews() {
-
         H = new QuadraticProbingHashTable<WordInfo>();
     }
-
-
 
     public String toString() {
         int LIMIT = 20;
         return name + "\n" + H.toString(LIMIT);
     }
 
-
     private String name;
     private QuadraticProbingHashTable<WordInfo> H;
 
-    public void readReviews(String filename)
-            throws FileNotFoundException, IOException {
+    public void readReviews(String filename) throws FileNotFoundException, IOException {
             BufferedReader in = new BufferedReader(new FileReader(filename));
             String name = null;
             String line;
@@ -40,26 +36,89 @@ public class Reviews {
                 System.out.println(r.toString());
             }
             System.out.println("Number of Reviews " +  line_count);
+    }
 
-        }
-        private static class ReviewInfo {
-            int score;
-            String[] words;
+    public void learnFromFile() throws FileNotFoundException {
+        File file = new File("movieReviews.txt");
+        Scanner sc = new Scanner(file);
 
-            // Constructors
-            ReviewInfo(int score, String[] words) {
-                this.score = score;
-                this.words = words;
+        boolean isNew = true;
+        int rating = 0;
+
+        while (sc.hasNext()) {
+            String word = sc.next().toLowerCase();
+            if (isNew) {
+                rating = Integer.parseInt(word);
+                isNew = false;
+            } else {
+                if (!word.equals(".")) {
+                    if (Character.isLetter(word.charAt(0))) {
+                        WordInfo info = new WordInfo(word);
+                        if(!H.contains(info)) {
+                            H.insert(info);
+                        } else {
+                            info.update(rating);
+                        }
+                    }
+                } else {
+                    isNew = true;
+                }
             }
+        }
+    }
 
-            public String toString() {
-                StringBuilder sb = new StringBuilder();
-                sb.append("Review " + score+ " Length of Review " + (words.length -1) + " ");
-                for (int i = 1; i < 11 & i < words.length; i++)
-                    sb.append(words[i] + " ");
-                return sb.toString();
+    public void handleUserReviewInfo() {
+        double sum = 0.0d;
+        int count = 0;
+        double average = 0.0d;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a review: ");
+
+        while (sc.hasNext()) {
+            String input = sc.next().toLowerCase();
+            if(!input.equals(".")) {
+                WordInfo info = new WordInfo(input);
+                WordInfo entry = H.find(info);
+                if(entry != null) {
+                    sum += entry.totalScore;
+                    count++;
+                    average = entry.getAverage();
+                }
+            } else {
+                break;
             }
         }
+
+        System.out.println("The review has an average value of " + average);
+
+        if (average < 1.75) {
+            System.out.println("Negative");
+        } else if (average >= 1.75 && average < 2.25) {
+            System.out.println("Neutral");
+        } else {
+            System.out.println("Positive");
+        }
+    }
+
+    private static class ReviewInfo {
+        int score;
+        String[] words;
+
+        // Constructors
+        ReviewInfo(int score, String[] words) {
+            this.score = score;
+            this.words = words;
+        }
+
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Review " + score+ " Length of Review " + (words.length -1) + " ");
+            for (int i = 1; i < 11 & i < words.length; i++)
+                sb.append(words[i] + " ");
+            return sb.toString();
+        }
+    }
 
     private static class WordInfo {
         int totalScore;
@@ -73,6 +132,12 @@ public class Reviews {
             numberOfOccurences = 0;
         }
 
+        public double getAverage() {
+            double total = totalScore;
+            double count = numberOfOccurences;
+            return total / count;
+        }
+
         public void update(int score){
             this.totalScore+=score;
             this.numberOfOccurences++;
@@ -84,21 +149,27 @@ public class Reviews {
     }
 
         public static void main (String[ ]args ){
-
+//            try {
+//                Reviews r1 = new Reviews();
+//                r1.readReviews("movieReviews.txt");
+//                System.out.println(r1);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            WordInfo w = new WordInfo("fat");
+//            w.update(4);
+//            System.out.println(w.toString());
 
             try {
                 Reviews r1 = new Reviews();
-                r1.readReviews("movieReviews.txt");
-                System.out.println(r1);
-
-
+                r1.learnFromFile();
+                r1.handleUserReviewInfo();
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            WordInfo w = new WordInfo("fat");
-            w.update(4);
-            System.out.println(w.toString());
+
         }
 
     }
